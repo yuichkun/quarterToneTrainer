7ceccbe25a67fc67b8a89b121794f94b35742554
Yuichi Yogo <yuichkun@gmail.com>
2018-10-17 01:06:19 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let MAX_INSTS = 10;
+let NOTES_RANGE = 39;
+let LEVEL = 3;
+$(document).ready(function () {
+  const synths = Array.from({ length: MAX_INSTS }, createSynth);
+  const wrapper = $('#wrapper');
+  function quiz() {
+    wrapper.empty();
+    let notes = pickRndNotes(LEVEL);
+    notes.forEach((note, i) => {
+      const synth = synths[i];
+      playNote(synth, note);
+      addImage(wrapper, note);
+    });
+  }
+  $(document).click(quiz);
+});
+
+function createSynth() {
+  return new Tone.Synth({
+    "oscillator": {
+      "type": "sine"
+    }
+  }).toMaster();
+}
+
+function addImage(wrapper, num) {
+  const img = $('<img class="score">')
+  img.attr('src', `assets/${num}.png`);
+  img.appendTo(wrapper);
+}
+
+function playNote(synth, noteNum) {
+  const midiNum = (noteNum / 2) + 60;
+  const freq = Tone.Midi(midiNum).toFrequency();
+  synth.triggerAttackRelease(freq, "2");
+}
+
+function pickRndNotes(noteCount) {
+  return Array.from({ length: noteCount }, () => Math.floor(Math.random() * NOTES_RANGE));
+}
